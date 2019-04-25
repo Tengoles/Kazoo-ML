b134174d71ed5aa40951458a72117035bc125b75
Tengoles <enzo.tng@gmail.com>
2019-04-25 14:25:12 -0300

    Update train_server.py

--- train_server.py
+++ train_server.py
@@ -11,12 +11,16 @@ app = Flask(__name__)
 @app.route('/api',methods=['POST'])
 def receive_data():
     # Get the data from the POST request.
-    data = request.get_json(force=True)
-    print("Data received")
-    dirPATH = model_settings.train_data_path
-    with open(dirPATH + time.strftime("%Y%m%d-%H%M%S") + data[0]['HUB'] + '.json', 'a') as fout:
-        json.dump(data, fout)
-    return jsonify({'result':'Success!'})
+    if request.is_json:
+	data = request.get_json(force=True)
+	print("Data received")
+	dirPATH = model_settings.train_data_path
+	with open(dirPATH + time.strftime("%Y%m%d-%H%M%S") + data[0]['HUB'] + '.json', 'a') as fout:
+	json.dump(data, fout)
+	return "JSON Received and stored!", 200
+    else:
+	# The request body wasn't JSON so return a 400 HTTP status code
+        return "Request was not JSON", 400	    
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", port=5000)
 	#app.run(port=5000, debug=True)
