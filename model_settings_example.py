6f07b2d6165abe9853aeb4aef90382e35edfd566
Ubuntu <ubuntu@ip-172-31-94-177.ec2.internal>
2019-04-30 19:26:31 +0000

    agregue prametro de direccion de directorio para data que se quiere predecir

--- model_settings_example.py
+++ model_settings_example.py
@@ -5,5 +5,7 @@ Classes = ['Sillon1', 'PasilloDerecha', 'PasilloIzquierda', 'Sillon2']  # estas 
 test_times = [('20190411-115300', '20190411-120000'), ('20190411-120400','20190411-121200'),
                   ('20190411-121400', '20190411-122300'), ('20190411-125700','20190411-130700')]  # rangos de tiempo en los que se realizo cada entrenamiento, uno por cada clase
 
-train_data_path = "/home/ubuntu/webserver/logs/"
+train_data_path = "/home/ubuntu/Kazoo-ML/train_data"
+
+predict_data_path = "/home/ubuntu/Kazoo-ML/predict_data"
 
