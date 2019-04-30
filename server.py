c4d6b54f5e16ea051d9b1a6c86d73b9428004432
Ubuntu <ubuntu@ip-172-31-94-177.ec2.internal>
2019-04-30 15:00:05 +0000

    cambio de nombre y poco mas

---
+++ server.py
@@ -0,0 +1,28 @@
+# Import libraries
+#import numpy as np
+#import pandas as pd
+from flask import Flask, request, jsonify
+import time
+import json
+import model_settings
+#import pickle
+app = Flask(__name__)
+
+@app.route('/train',methods=['POST'])
+def receive_data():
+    # Get the data from the POST request.
+    data = request.get_json(force=True)
+    print("Data received")
+    dirPATH = model_settings.train_data_path
+    with open(dirPATH + time.strftime("%Y%m%d-%H%M%S") + "_"  + data[0]['HUB'] + '.json', 'a') as fout:
+        json.dump(data, fout)
+    app.logger.info('%s train data received successfully', time.strftime("%Y%m%d-%H%M%S") + "_"  + data[0]['HUB'] + '.json')
+    return "JSON Received and stored!", 200
+
+
+@app.route('/predict', methods=['POST'])
+def 
+
+if __name__ == '__main__':
+	app.run(host="0.0.0.0", port=5000)
+	#app.run(port=5000, debug=True)
