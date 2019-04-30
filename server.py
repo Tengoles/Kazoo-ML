1b5c40df0467113c3ac8b77cf36c82c58e445fc9
Ubuntu <ubuntu@ip-172-31-94-177.ec2.internal>
2019-04-30 19:26:19 +0000

    esta version recibe y guarda post de json en train y en predict

--- server.py
+++ server.py
@@ -21,8 +21,18 @@ def receive_data():
 
 
 @app.route('/predict', methods=['POST'])
-def 
+def process_file():
+    # Get the data from the POST request.
+    data = request.get_json(force=True)
+    print("Data received")
+    dirPATH = model_settings.predict_data_path
+    with open(dirPATH + time.strftime("%Y%m%d-%H%M%S") + "_"  + data[0]['HUB'] + '.json', 'a') as fout:
+        json.dump(data, fout)
+    app.logger.info('%s predict data received successfully', time.strftime("%Y%m%d-%H%M%S") + "_"  + data[0]['HUB'] + '.json')
+    return "JSON Received and stored!", 200
 
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=5000)
+    import logging
+    logging.basicConfig(filename='messages.log',level=logging.DEBUG)
+    app.run(host="0.0.0.0", port=5000)
 	#app.run(port=5000, debug=True)
