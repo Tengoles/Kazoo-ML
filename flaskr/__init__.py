8fe9066a023cc3b331130e73931cf6852a94e761
Ubuntu <ubuntu@ip-172-31-85-151.ec2.internal>
2019-05-15 14:18:07 +0000

    sacar cosas que no sirven y cambiar mensaje de /home

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,8 +1,6 @@
 import os
 #import logging
 from flask import Flask, request, jsonify
-#import time
-#import json
 #import model_settings
 
 def create_app(test_config=None):
@@ -13,18 +11,6 @@ def create_app(test_config=None):
         #DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
-    #if test_config is None:
-        # load the instance config, if it exists, when not testing
-        #app.config.from_pyfile('config.py', silent=True)
-    #else:
-        # load the test config if passed in
-    #app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    #try:
-    #    os.makedirs(app.instance_path)
-    #except OSError:
-    #    pass
     from . import posts
     app.register_blueprint(posts.bp)
 #    import logging
@@ -32,11 +18,5 @@ def create_app(test_config=None):
     # here goes the routes
     @app.route("/home")
     def hello():
-        return "Hello World!"
+        return "Plataforma de Machine Learning de Kazoo"
     return app
-
-#app = create_app()
-
-#if __name__ == "__main__":
-    #app = create_app()
- #   app.run(host='0.0.0.0')
