b7e12508359b066d96e1ec19418261bdd2ec8636
Tengoles <enzo.tng@gmail.com>
2019-07-24 21:02:42 +0000

    cambios para que funcione aunque en realidad no funciona

--- test_tools/delete_json_test.py
+++ test_tools/delete_json_test.py
@@ -6,12 +6,10 @@ received_files_path = os.path.join(received_files_path, "predict_data")
 print(received_files_path)
 #en received_files_path tengo el path para los archivos que el servidor recibe y todavia no proceso
 
-script_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+script_path = os.path.dirname(os.path.abspath(__file__))
 files_path = os.path.join(script_path, "test_files")
-test_files = os.listdir(files_path)
-test_files = os.listdir(received_files_path)
-print(test_files)
-#en test_files tengo los nombres de los archivos que se mandan para hacer test
+test_files = os.listdir(files_path) #en test_files tengo los nombres de los archivos que se mandan para hacer test
+#print(test_files)
 for file in test_files:
     print("Deleting: %s"%(os.path.join(received_files_path, file)))
     os.remove(os.path.join(received_files_path, file))
