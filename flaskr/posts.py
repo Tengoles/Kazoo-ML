ac04a97b53c4e5cd1dbb80a04ae9fe35557581ef
Tengoles <enzo.tng@gmail.com>
2019-08-14 19:28:11 +0000

    quedo agregado feature de que sepa procesar datos comprimidos y cambio de forma en que recibe los json sin comprimir

--- flaskr/posts.py
+++ flaskr/posts.py
@@ -2,29 +2,31 @@ from flask import Blueprint, request, current_app
 from . import model_settings
 import json
 import time
+import gzip
+import zlib
+#import StringIO
 
 bp = Blueprint('posts', __name__, url_prefix='/posts')
 import logging
 logging.basicConfig(filename=model_settings.LOG_FILE, level=logging.DEBUG)
 
-@bp.route('/train',methods=['POST'])
-def receive_data():
-    # Get the data from the POST request
-    data = request.get_json(force=True)
-    print("Data received")
-    dirPATH = model_settings.train_data_path
-    with open(dirPATH + time.strftime("%Y%m%d-%H%M%S") + "_"  + data[0]['HUB'] + '.json', 'a') as fout:
-        json.dump(data, fout)
-    current_app.logger.info('%s train data received successfully', time.strftime("%Y%m%d-%H%M%S") + "_"  + data[0]['HUB'] + '.json')
-    return "JSON Received and stored!", 200
-
 @bp.route('/predict', methods=['POST'])
 def process_file():
     # Get the data from the POST request.
-    data = request.get_json(force=True)
-    print("Data received")
     dirPATH = model_settings.predict_data_path
-    with open(dirPATH + time.strftime("%Y%m%d-%H%M%S") + "_"  + data[0]['HUB'] + '.json', 'a') as fout:
-        json.dump(data, fout)
-    current_app.logger.info('%s predict data received successfully', time.strftime("%Y%m%d-%H%M%S") + "_"  + data[0]['HUB'] + '.json')
+    if request.is_json:
+        data = request.get_json(force=True)
+        filename = time.strftime("%Y%m%d-%H%M%S") + "_"  + data['hub'] + '.json'
+        with open(dirPATH + filename, 'a') as fout:
+            json.dump(data['data'], fout)
+        current_app.logger.info('%s predict data received successfully', filename)
+    else:
+        print("request.is_json: " + str(request.is_json))
+        data = request.data
+        decompressed_data = zlib.decompress(data)
+        data = json.loads(zlib.decompress(data).decode("UTF-8"))
+        filename = time.strftime("%Y%m%d-%H%M%S") + "_"  + data[0]['HUB'] + '.json'
+        with open(dirPATH + filename, 'a') as fout:
+            json.dump(data, fout)
+        current_app.logger.info('%s predict data received successfully', filename)
     return "JSON Received and stored!", 200
