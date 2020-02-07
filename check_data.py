41a13111f9c3e9376d80577a0f67112e2b00f9e6
Tengoles <enzo.tng@gmail.com>
2020-02-07 16:46:17 -0300

    cambios

--- check_data.py
+++ check_data.py
@@ -1,33 +1,81 @@
 import os
-from flaskr import model_settings
 from datetime import datetime, timedelta
-import json
 import pandas as pd
+import psycopg2
 import sys
 
-def load_json_dates(date1, date2, directory):
-	date1 = datetime.strptime(date1, '%Y%m%d-%H%M%S') + timedelta(hours=3)
-	date2 = datetime.strptime(date2, '%Y%m%d-%H%M%S') + timedelta(hours=3)
-	data_directory = model_settings.predict_data_path + directory + "/"
-	files = os.listdir(data_directory)
-	files.sort()
-	data_cruda = []
-	for f in files:
-		file_date = datetime.strptime(f[0:15],  '%Y%m%d-%H%M%S')
-		if (".json" in f and file_date >= date1 and file_date <= date2):
-			print(f)
-			with open(data_directory + f) as json_file:
-				data_cruda.extend(json.loads(json_file.read()))
-	DF = pd.DataFrame(data_cruda)
-	DF['fechahora'] = pd.to_datetime(DF['fechahora'], format="%Y-%m-%d %H:%M:%S.%f")
-	return DF.sort_values(by='fechahora')
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
 
-cur, conn = model_settings.connect()
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(host="34.196.210.24", database="kazoo", user="kazoo",
+                                password="production.K4zoo2018*")
+
+        # create a cursor
+        cur = conn.cursor()
+
+        # execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+
+        # close the communication with the PostgreSQL
+        #cur.close()
+        return cur, conn
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+cur, conn = connect()
 try:
-	query = """SELECT "MAC", fechahora, "Zona" FROM public."PCSEST" WHERE public."PCSEST".fechahora >= '2019-11-19 00:00:01' AND public."PCSEST".fechahora <= '2019-11-20 00:00:01';"""
+	#query = """SELECT fechahora, mac, rssi, evento FROM public."IPG" WHERE public."IPG".fechahora >= '2019-12-04 00:00:00' AND public."IPG".fechahora <= '2019-12-05 00:00:00';"""
+	query = """SELECT "MAC", fechahora, "Zona" FROM public."PCSEST" WHERE public."PCSEST".fechahora >= '2019-12-07 00:00:00' AND public."PCSEST".fechahora <= '2019-12-10 00:00:00';"""
 	cur.execute(query)
 	response = cur.fetchall()
 	data = pd.DataFrame(columns=["MAC", "fechahora", "Zona"], data=response)
+	#data = pd.DataFrame(columns=["fechahora", "mac", "rssi", "evento"], data=response)
+	data = data[data.Zona == "A2"]
+	#data = data[data.evento == "IPG"]
+	zonas = data.Zona.unique()
+	data.reset_index(inplace=True)
+	first_date = datetime(data['fechahora'][0].year, data['fechahora'][0].month, data['fechahora'][0].day, 0, 0)
+	last_date = datetime(data['fechahora'][len(data) - 1].year, data['fechahora'][len(data) - 1].month,
+						 data['fechahora'][len(data) - 1].day, 0, 0)
+	ventana = 600
+	print("Ventana de visita recurrente: %s"%ventana)
+	macs_recurrentes = []
+	for day in range(last_date.day - first_date.day + 1):
+		print(str(first_date))
+		data_day = data[(data['fechahora'] > first_date) & (data['fechahora'] <= first_date + timedelta(days=1))]
+		visitas_unicas = 0
+		visitas_totales = 0
+		recurrencias = 0
+		for zona in zonas:
+			print("Zona: %s"%(zona))
+			data_zona = data_day[data_day["Zona"] == zona]
+			visitas_unicas += len(data_zona.MAC.unique())
+			for index, mac in enumerate(data_day.MAC.unique()):
+				df_mac = data_zona[data_zona["MAC"] == mac].sort_values(by="fechahora")
+				new_mac = True
+				for j in range(len(df_mac) - 1):
+					probe_delta = df_mac.iloc[j + 1].fechahora - df_mac.iloc[j].fechahora
+					if probe_delta > timedelta(seconds=ventana):
+						if new_mac == True:
+							recurrencias += 1
+							macs_recurrentes.append(mac)
+							new_mac = False
+						visitas_totales += 1
+		visitas_totales += visitas_unicas
+		print("Visitas unicas: %s \n Visitas totales: %s \nVisitantes recurrentes: %s\n ---------------------" % (str(visitas_unicas),
+																								visitas_totales, recurrencias))
+		first_date = first_date + timedelta(days=1)
+	#for mac in macs_recurrentes:
+
 	cur.close()
 	conn.close()
 except Exception as e:
