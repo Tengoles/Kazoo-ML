8e53fe3237a79ae37960c8edcab512113c481d89
Tengoles <enzo.tng@gmail.com>
2020-02-10 13:20:17 -0300

    borrar scripts inutiles

--- local_scripts/PCSEST_query.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-import pandas as pd
-from flaskr import model_settings
-
-engine = create_engine(model_settings.engine_string)
-
-query = """SELECT * FROM public."PCSEST" WHERE fechahora between '2019-10-18 11:30:03.655306' and '2019-10-18 11:33:03.655306' """
-df = pd.read_sql(query, engine)
