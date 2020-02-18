c37c68e5ba0956793167665c3e75fef2ab78d903
Ubuntu <ubuntu@ip-172-31-81-19.ec2.internal>
2020-02-18 20:15:53 +0000

    agregar archivos para que la pagina quede linda y siguiendo ideologia de Django

--- Kazoo_ML_root/Kazoo_ML/settings.py
+++ Kazoo_ML_root/Kazoo_ML/settings.py
@@ -126,7 +126,7 @@ ROOT_URLCONF = 'Kazoo_ML.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'Kazoo_ML', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -192,5 +192,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Kazoo_ML', 'static')]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
--- Kazoo_ML_root/pages/views.py
+++ Kazoo_ML_root/pages/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from . models import Page
 
-def index(request):
-    return HttpResponse("<h1>Homepage de Kazoo-ML</h1>")
+def index(request, pagename):
+    pagename = '/' + pagename
+    pg = Page.objects.get(permalink=pagename)
+    context = {
+            'title': pg.title,
+            'content': pg.bodytext,
+            'last_updated': pg.update_date,
+            'page_list': Page.objects.all(),
+    }
+    return render(request, 'pages/page.html', context)
 
 # Create your views here.
